f0640233b3750a524fcc9a8920d3402e3cbcb2a9
DeathLust <dmitrii.lebedev@gmail.com>
2015-10-03 15:59:55 +0300

    Re-using existing Uri to feed activity with updated package string. Simply call new intent on existing activity.

--- app/src/main/java/ru/spb/deathlust/packageupdatedapp/MainActivity.java
+++ app/src/main/java/ru/spb/deathlust/packageupdatedapp/MainActivity.java
@@ -1,11 +1,7 @@
 package ru.spb.deathlust.packageupdatedapp;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,57 +9,39 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String INTENT = "ru.spb.deathlust.packageupdatedapp.PACKAGE_ADDED";
-    public static final String PACKAGE = "package";
     private String packageStr = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        packageStr = "com.google.android.gm";
         setContentView(R.layout.activity_main);
         updateControls();
-        LocalBroadcastManager.
-                getInstance(getApplicationContext()).
-                registerReceiver(messageReceiver, new IntentFilter(INTENT));
     }
 
     private void updateControls() {
         TextView textView = (TextView)findViewById(R.id.packageTextView);
         textView.setText(packageStr);
         Button button = (Button)findViewById(R.id.googlePlayButton);
-        button.setEnabled(!packageStr.equals(""));
+        button.setEnabled(!"".equals(packageStr));
     }
 
-    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String prefix = "package:";
-            String data = intent.getStringExtra(PACKAGE);
-            if (data.startsWith(prefix)) {
-                packageStr = data.substring(prefix.length());
-                updateControls();
-            }
-        }
-    };
-
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(messageReceiver);
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if ("package".equals(intent.getScheme())) {
+            packageStr = intent.getData().getSchemeSpecificPart();
+            setIntent(intent);
+            updateControls();
+        }
     }
 
     public void googlePlayButtonClick(View view) {
         String url;
-
         try {
             getPackageManager().getPackageInfo("com.android.vending", 0);
-
             url = "market://details?id=" + packageStr;
         } catch ( final Exception e ) {
             url = "http://play.google.com/store/apps/details?id=" + packageStr;
         }
-
         final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
     }
--- app/src/main/java/ru/spb/deathlust/packageupdatedapp/PackageUpdatedReceiver.java
+++ app/src/main/java/ru/spb/deathlust/packageupdatedapp/PackageUpdatedReceiver.java
@@ -3,17 +3,12 @@ package ru.spb.deathlust.packageupdatedapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
 
 public class PackageUpdatedReceiver extends BroadcastReceiver {
-    public PackageUpdatedReceiver() {
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        String packageStr = intent.getDataString();
-        Intent signal = new Intent(MainActivity.INTENT);
-        signal.putExtra(MainActivity.PACKAGE, packageStr);
-        LocalBroadcastManager.getInstance(context.getApplicationContext()).sendBroadcast(signal);
+        context.startActivity(new Intent(context, MainActivity.class)
+                .setData(intent.getData())
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
     }
 }
